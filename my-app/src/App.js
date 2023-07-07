24a672f02a8791d83c01e525fa9b70eaafeef080
Max <maxtretter@tutanota.com>
2023-07-07 12:14:35 +0200

    board: js hexagon array and shuffle algrorithm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+
+
+import './App.scss';
+
 
 function App() {
+  // making the array with all the content for the hexagons
+  const wood = new Array(4).fill('wood');
+  const weat = new Array(4).fill('weat');
+  const sheep = new Array(4).fill('sheep');
+  const stone = new Array(3).fill('stone');
+  const clay = new Array(3).fill('clay');
+  const allElementsInitial = [...wood, ...weat, ...sheep, ...stone, ...clay, 'desert'];
+
+  const [allElements, setAllElements] = useState(allElementsInitial);
+
+  let shuffleArray = () => {
+    const shuffledElements = [...allElements];
+    for (let i = shuffledElements.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledElements[i], shuffledElements[j]] = [shuffledElements[j], shuffledElements[i]];
+    }
+    setAllElements(shuffledElements)
+    console.log('hello');
+    console.log('array:', allElements)
+
+  };
+
+  // shuffleArray(allElements);
+  // console.log(allElements)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={shuffleArray} variant="contained">Shuffle</Button>
+      <div className="main">
+        <div className="container three">
+          {allElements.slice(0, 3).map(element => <div><div>{element}</div></div>)}
+        </div>
+        <div className="container four">
+          {allElements.slice(3, 7).map(element => <div><div>{element}</div></div>)}
+        </div>
+        <div className="container">
+          {allElements.slice(7, 12).map(element => <div><div>{element}</div></div>)}
+        </div>
+        <div className="container four">
+          {allElements.slice(12, 16).map(element => <div><div>{element}</div></div>)}
+        </div>
+        <div className="container three">
+          {allElements.slice(16, 19).map(element => <div><div>{element}</div></div>)}
+        </div>
+      </div>
     </div>
   );
 }
