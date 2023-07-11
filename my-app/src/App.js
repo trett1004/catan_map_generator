9f8a42fbfda5fd7360e59e5e9794fcc838a7dac5
Max <maxtretter@tutanota.com>
2023-07-11 14:31:48 +0200

    board: fix '-1 on desert' bug remove all comments from old hexagon in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
-import Btn from './components/Button/Btn.js';
-import Hexagon from './components/Hexagon/Hexagon.js';
+import Btn from './components/Btn/Btn.js';
+import HexagonRow from './components/HexagonRow/HexagonRow.js';
+import GreenAlert from './components/Alert/Alert.js';
 
 import './App.scss';
-import GreenAlert from './components/Alert/Alert.js';
+
 
 
 function App() {
-  // making the array with the background images for the hexagons
+  // creating the array with the background images for the hexagons
   const wood = new Array(4).fill('wood');
   const weat = new Array(4).fill('weat');
   const sheep = new Array(4).fill('sheep');
   const stone = new Array(3).fill('stone');
   const clay = new Array(3).fill('clay');
   const allElementsInitial = [...wood, ...weat, ...sheep, ...stone, ...clay, 'desert'];
-  // making the array for the numbers on each hexagon
+  // create the array for the numbers on each hexagon
   const numbersInitial = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12, -1];
 
+  // Hooks
   const [numbers, setNumbers] = useState(numbersInitial)
   const [allElements, setAllElements] = useState(allElementsInitial);
   const [alert, setAlert] = useState(false);
+  const [data, setData] = React.useState(null);
 
   // shuffle the backgroundimages
   const shuffleBackground = () => {
     const shuffledElements = [...allElements];
     for (let i = shuffledElements.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
+      // swap
       [shuffledElements[i], shuffledElements[j]] = [shuffledElements[j], shuffledElements[i]];
     }
     setAllElements(shuffledElements);
@@ -38,21 +42,20 @@ function App() {
     const shuffledNumbers = [...numbers]
     for (let i = shuffledNumbers.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
+      // swap
       [shuffledNumbers[i], shuffledNumbers[j]] = [shuffledNumbers[j], shuffledNumbers[i]];
     }
     const x = shuffledElements.findIndex(element => element === 'desert');
     const y = shuffledNumbers.findIndex(element => element === -1);
-    // console.log(x, y)
     [shuffledNumbers[x], shuffledNumbers[y]] = [shuffledNumbers[y], shuffledNumbers[x]];
-
     setNumbers(shuffledNumbers);
   }
 
   const getClass = (idx) => {
+    console.log('numbers[idx] App', numbers[idx])
     return numbers[idx] === -1 ? "hexagonBg hidden" : "hexagonBg"
   }
-
-  const [data, setData] = React.useState(null);
+  // API check
 
   React.useEffect(() => {
     fetch("/api")
@@ -60,8 +63,9 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
-  const handleRateClick = () => {
 
+  // Save board to db
+  const handleRateClick = () => {
     fetch('/rate', {
       method: 'POST',
       body: JSON.stringify({
@@ -90,32 +94,26 @@ function App() {
     <div className="App">
       <Btn onClick={handleRateClick} content="RATE"/>
       <div>{alert ? <GreenAlert content="Rating received" severity="success" /> : <></>}</div>
-      <p>{!data ? "Loading..." : data}</p>
+
       <Btn onClick={shuffleBackground} variant="contained" content="SHUFFLE" />
       <div className="hexagonField">
         {/* backgroundimages-array is sliced and numbers-arrayindex is called for each row.
         Background images and numbers from the respective arrays are implemented after shuffle*/}
-        {/* <div className="singleHexagon three">
-          {allElements.slice(0, 3).map((element, idx) => <div className={element}><div className={getClass(idx)}>{numbers[idx]}</div></div>)}
-        </div> */}
-        <Hexagon allElements={allElements} numbers={numbers} arrayStart={0} arrayEnd={3} idxAdder = {0} className="singleHexagon three"/>
-        <Hexagon allElements={allElements} numbers={numbers} arrayStart={3} arrayEnd={7} idxAdder = {3} className="singleHexagon four"/>
-        {/* <div className="singleHexagon four">
-          {allElements.slice(3, 7).map((element, idx) => <div className={element}><div className={getClass(idx + 3)}>{numbers[idx + 3]}</div></div>)}
-        </div> */}
-        <Hexagon allElements={allElements} numbers={numbers} arrayStart={7} arrayEnd={12} idxAdder = {7} className="singleHexagon"/>
-        {/* <div className="singleHexagon">
-          {allElements.slice(7, 12).map((element, idx) => <div className={element}><div className={getClass(idx + 7)}>{numbers[idx + 7]}</div></div>)}
-        </div> */}
-        <Hexagon allElements={allElements} numbers={numbers} arrayStart={12} arrayEnd={16} idxAdder = {12} className="singleHexagon four"/>
-        {/* <div className="singleHexagon four">
-          {allElements.slice(12, 16).map((element, idx) => <div className={element}><div className={getClass(idx + 12)}>{numbers[idx + 12]}</div></div>)}
-        </div> */}
-        <Hexagon allElements={allElements} numbers={numbers} arrayStart={16} arrayEnd={19} idxAdder = {16} className="singleHexagon three"/>
-        {/* <div className="singleHexagon three">
-          {allElements.slice(16, 19).map((element, idx) => <div className={element}><div className={getClass(idx + 16)}>{numbers[idx + 16]}</div></div>)}
-        </div> */}
+
+        <HexagonRow allElements={allElements} numbers={numbers} arrayStart={0} arrayEnd={3} idxAdder = {0} className="singleHexagon three"/>
+
+        <HexagonRow allElements={allElements} numbers={numbers} arrayStart={3} arrayEnd={7} idxAdder={3} className="singleHexagon four"/>
+
+        <HexagonRow allElements={allElements} numbers={numbers} arrayStart={7} arrayEnd={12} idxAdder = {7} className="singleHexagon"/>
+
+        <HexagonRow allElements={allElements} numbers={numbers} arrayStart={12} arrayEnd={16} idxAdder = {12} className="singleHexagon four"/>
+
+        <HexagonRow allElements={allElements} numbers={numbers} arrayStart={16} arrayEnd={19} idxAdder = {16} className="singleHexagon three"/>
+
       </div>
+      <br></br>
+      <br></br>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 
---
+++ my-app/src/components/HexagonRow/HexagonRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const getClass = (idx, idxAdder, numbers) => {
+    return numbers[idx+idxAdder] === -1 ? "hexagonBg hidden" : "hexagonBg";
+}
+
+const Hexagon = ({ allElements, numbers, className, idxAdder, arrayStart, arrayEnd,...props }) => {
+    return (
+        <div className={className}>
+            {allElements.slice(arrayStart, arrayEnd).map((element, idx) => (
+                <div className={element}>
+                    <div className={getClass(idx, idxAdder, numbers)}>{numbers[idx + idxAdder]}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Hexagon;
+
+// Old code from App.js before refactoring
+{/* <div className="singleHexagon four">
+    {allElements.slice(3, 7).map((element, idx) => <div className={element}><div className={getClass(idx + 3)}>{numbers[idx + 3]}</div></div>)}
+</div> */}
