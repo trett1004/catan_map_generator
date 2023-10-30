15e5dc277d3abb177803ff4089a8a23a47407c77
Max <maxtretter@tutanota.com>
2023-10-30 10:34:08 +0100

    testing: write test for component App (root)

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,11 +1,40 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App"; // No need to import MainSection separately
+import { landfieldsArr, fieldNumbersArr } from "./helpers/create_board";
 
-describe("Application", () => {
-  test("App renders correctly", () => {
+describe("App Integration Test", () => {
+  test("has 19 landfields", () => {
     render(<App />);
-    const heading = screen.getByText("Catan Map Gen");
-    expect(heading).toBeInTheDocument();
+    const landfields = screen.getAllByTestId("hexagon-land-field");
+    expect(landfields.length).toBe(19);
+  });
+  test("Shuffels the landfields correctly", () => {
+    render(<App />);
+    const shuffleBtn = screen.getByText(/SHUFFLE THE FIELDS/i);
+    expect(shuffleBtn).toBeInTheDocument();
+    fireEvent.click(shuffleBtn);
+
+    const shuffledLandfields = screen.getAllByTestId("hexagon-land-field");
+    expect(shuffledLandfields[18]).not.toHaveClass("desert");
+    const newLandfieldArrBgImg = shuffledLandfields.map(
+      (element, index) => element.className
+    );
+    expect(newLandfieldArrBgImg).not.toEqual(landfieldsArr);
+    expect(newLandfieldArrBgImg.length).toEqual(19);
+
+    const newLandfieldArrNumbers = shuffledLandfields.map((element, index) =>
+      parseInt(element.textContent)
+    );
+    expect(newLandfieldArrNumbers).not.toEqual(fieldNumbersArr);
+    expect(newLandfieldArrNumbers.length).toEqual(19);
+  });
+  test("Renders the shuffled map name correctly", () => {
+    render(<App />);
+    const shuffleBtn = screen.getByText(/SHUFFLE THE FIELDS/i);
+    expect(shuffleBtn).toBeInTheDocument();
+    fireEvent.click(shuffleBtn);
+    const mapName = screen.getByText(/map:.+/i);
+    expect(mapName.textContent).not.toEqual("Map:");
   });
 });
